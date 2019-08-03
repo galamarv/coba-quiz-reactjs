b26e42784b8809529226d227ffc556ccbd0a1729
Fattah <fatah@gians-MacBook-Pro.local>
2019-08-04 03:24:54 +0700

    tes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from 'react';
 import './App.css';
 import Quiz from 'react-quiz-component';
 
-import { quiz } from './quiz';
+import {quiz} from './quiz';
+import {quiz1} from './quiz1';
+import {quiz2} from './quiz2';
+
+
 
 
 function App() {
+  let acak;
+  let angka = Math.floor(Math.random() * 3);  
+  if (angka  === 0) {
+    acak = <Quiz  quiz={quiz} shuffle={true} />
+  }
+  else if (angka === 1) {
+    acak = <Quiz  quiz={quiz1} shuffle={true} />
+  }
+  else if (angka > 1) {
+    acak = <Quiz  quiz={quiz2} shuffle={true} />
+  }
   return (
     <div  >
-       <Quiz className="myImage" quiz={quiz} shuffle={true} showDefaultResult={false}/>
+       {acak}
     </div>
   );
 }
---
+++ src/quiz2.js
@@ -0,0 +1,77 @@
+export const quiz2 = {
+    "quizTitle": "React Quiz Component Demo 3",
+    "quizSynopsis": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim",
+    "questions": [
+      {
+        "question": "How can you access the state of a component from inside of a member function?",
+        "questionType": "text",
+        "answers": [
+          "this.getState()",
+          "this.prototype.stateValue",
+          "this.state",
+          "this.values"
+        ],
+        "correctAnswer": "3",
+        "messageForCorrectAnswer": "Correct answer. Good job.",
+        "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+        "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "point": "20"
+      },
+      {
+        "question": "ReactJS is developed by _____?",
+        "questionType": "text",
+        "answers": [
+          "Google Engineers",
+          "Facebook Engineers"
+        ],
+        "correctAnswer": "2",
+        "messageForCorrectAnswer": "Correct answer. Good job.",
+        "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+        "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "point": "20"
+      },
+      {
+        "question": "ReactJS is an MVC based framework?",
+        "questionType": "text",
+        "answers": [
+          "True",
+          "False"
+        ],
+        "correctAnswer": "2",
+        "messageForCorrectAnswer": "Correct answer. Good job.",
+        "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+        "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "point": "20"
+      },
+      {
+        "question": "Which of the following concepts is/are key to ReactJS?",
+        "questionType": "text",
+        "answers": [
+          "Component-oriented design",
+          "Event delegation model",
+          "Both of the above"
+        ],
+        "correctAnswer": "3",
+        "messageForCorrectAnswer": "Correct answer. Good job.",
+        "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+        "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "point": "20"
+      },
+      {
+        "question": "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        "questionType": "photo",
+        "answers": [
+          "http://stage48.net/wiki/images/thumb/d/de/KitaharaRieNIII2018.jpg/192px-KitaharaRieNIII2018.jpg",
+          "http://stage48.net/wiki/images/thumb/d/de/KitaharaRieNIII2018.jpg/192px-KitaharaRieNIII2018.jpg",
+          "http://stage48.net/wiki/images/thumb/d/de/KitaharaRieNIII2018.jpg/192px-KitaharaRieNIII2018.jpg",
+          "http://stage48.net/wiki/images/thumb/d/de/KitaharaRieNIII2018.jpg/192px-KitaharaRieNIII2018.jpg"
+        ],
+        "correctAnswer": "1",
+        "messageForCorrectAnswer": "Correct answer. Good job.",
+        "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+        "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "point": "20"
+      }
+    ]
+  } 
+
