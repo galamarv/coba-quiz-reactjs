659ac2ee97dca45f0a7340efba7497848cb0dcd2
Fattah <fatah@gians-MacBook-Pro.local>
2019-08-04 20:57:10 +0700

    commit

--- src/quiz.js
+++ src/quiz.js
@@ -1,38 +1,39 @@
 export const quiz =  {
-  "quizTitle": "Quiz UB48",
+  "quizTitle": "Quiz Dai-Toku UB",
   "quizSynopsis": "Jawablah Pertanyaan Berikut",
   "questions": [
     {
       "questionType": "photo",
-      "question": "Cover dari Teacher Teacher Type B (Limited Edition) adalah",
+      "question": "Manakah Serial yang lebih dulu tayang",
       "answers": [
-        "http://stage48.net/wiki/images/5/51/AKB4852RegA.jpg",
-        "http://stage48.net/wiki/images/6/64/AKB4852LimB.jpg",
-        "http://stage48.net/wiki/images/1/16/AKB4852LimC.jpg"
+        "https://vignette.wikia.nocookie.net/ultra/images/f/fe/UQDF.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/6/66/Ougonkishigaro.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/7/78/Kamenrider06_hibiki_300.jpg"
       ],
-      "correctAnswer": "2",
+      "correctAnswer": "1",
       "point" : "50"
     },
     {
-      "question": "Sebutkan Single ke-33 AKB48",
+      "question": "Sebutkan Kamen Rider Pertama yang tayang di era Reiwa",
       "questionType": "text",
       "answers": [
-        "Heart Ereki",
-        "Sayonara Crawl ",
-        "So long!"
+        "Zero",
+        "Zero One",
+        "Zeronos",
+        "Zero 0ne"
       ],
-      "correctAnswer": "1",
+      "correctAnswer": "2",
       "point" : "50"
     }
   ],
   "appLocale": {
     "landingHeaderText": "<questionLength> Pertanyaan",
-    "question": "Question",
-    "startQuizBtn": "Start Quiz",
-    "resultFilterAll": "All",
-    "resultFilterCorrect": "Correct",
-    "resultFilterIncorrect": "Incorrect",
-    "nextQuestionBtn": "Next",
-    "resultPageHeaderText": "You have completed the quiz. You got <correctIndexLength> out of <questionLength> questions."
+    "question": "Pertanyaan",
+    "startQuizBtn": "Mulai Quiz",
+    "resultFilterAll": "Semua",
+    "resultFilterCorrect": "Benar",
+    "resultFilterIncorrect": "Salah",
+    "nextQuestionBtn": "Lanjut",
+    "resultPageHeaderText": "Anda benar <correctIndexLength> pertanyaan dari total <questionLength> pertanyaan."
   } 
 }
--- src/quiz1.js
+++ src/quiz1.js
@@ -1,77 +1,39 @@
 export const quiz1 = {
-    "quizTitle": "React Quiz Component Demo 2",
-    "quizSynopsis": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim",
-    "questions": [
-      {
-        "question": "How can you access the state of a component from inside of a member function?",
-        "questionType": "text",
-        "answers": [
-          "this.getState()",
-          "this.prototype.stateValue",
-          "this.state",
-          "this.values"
-        ],
-        "correctAnswer": "3",
-        "messageForCorrectAnswer": "Correct answer. Good job.",
-        "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-        "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "point": "20"
-      },
-      {
-        "question": "ReactJS is developed by _____?",
-        "questionType": "text",
-        "answers": [
-          "Google Engineers",
-          "Facebook Engineers"
-        ],
-        "correctAnswer": "2",
-        "messageForCorrectAnswer": "Correct answer. Good job.",
-        "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-        "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "point": "20"
-      },
-      {
-        "question": "ReactJS is an MVC based framework?",
-        "questionType": "text",
-        "answers": [
-          "True",
-          "False"
-        ],
-        "correctAnswer": "2",
-        "messageForCorrectAnswer": "Correct answer. Good job.",
-        "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-        "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "point": "20"
-      },
-      {
-        "question": "Which of the following concepts is/are key to ReactJS?",
-        "questionType": "text",
-        "answers": [
-          "Component-oriented design",
-          "Event delegation model",
-          "Both of the above"
-        ],
-        "correctAnswer": "3",
-        "messageForCorrectAnswer": "Correct answer. Good job.",
-        "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-        "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "point": "20"
-      },
-      {
-        "question": "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
-        "questionType": "photo",
-        "answers": [
-          "http://stage48.net/wiki/images/thumb/d/de/KitaharaRieNIII2018.jpg/192px-KitaharaRieNIII2018.jpg",
-          "http://stage48.net/wiki/images/thumb/d/de/KitaharaRieNIII2018.jpg/192px-KitaharaRieNIII2018.jpg",
-          "http://stage48.net/wiki/images/thumb/d/de/KitaharaRieNIII2018.jpg/192px-KitaharaRieNIII2018.jpg",
-          "http://stage48.net/wiki/images/thumb/d/de/KitaharaRieNIII2018.jpg/192px-KitaharaRieNIII2018.jpg"
-        ],
-        "correctAnswer": "1",
-        "messageForCorrectAnswer": "Correct answer. Good job.",
-        "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-        "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "point": "20"
-      }
-    ]
+  "quizTitle": "Quiz Dai-Toku UB",
+  "quizSynopsis": "Jawablah Pertanyaan Berikut",
+  "questions": [
+    {
+      "questionType": "photo",
+      "question": "Manakah Serial yang lebih dulu tayang",
+      "answers": [
+        "https://vignette.wikia.nocookie.net/ultra/images/f/fe/UQDF.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/6/66/Ougonkishigaro.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/7/78/Kamenrider06_hibiki_300.jpg"
+      ],
+      "correctAnswer": "1",
+      "point" : "50"
+    },
+    {
+      "question": "Sebutkan Kamen Rider Pertama yang tayang di era Reiwa",
+      "questionType": "text",
+      "answers": [
+        "Zero",
+        "Zero One",
+        "Zeronos",
+        "Zero 0ne"
+      ],
+      "correctAnswer": "2",
+      "point" : "50"
+    }
+  ],
+  "appLocale": {
+    "landingHeaderText": "<questionLength> Pertanyaan",
+    "question": "Pertanyaan",
+    "startQuizBtn": "Mulai Quiz",
+    "resultFilterAll": "Semua",
+    "resultFilterCorrect": "Benar",
+    "resultFilterIncorrect": "Salah",
+    "nextQuestionBtn": "Lanjut",
+    "resultPageHeaderText": "Anda benar <correctIndexLength> pertanyaan dari total <questionLength> pertanyaan."
   } 
-
+}
